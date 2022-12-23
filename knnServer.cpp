db44f4b94e4c3453abb834f23e1f7a06091aecf7
GLPRL <galpearl24@gmail.com>
2022-12-23 15:56:42 +0200

    Port validation checks and loop modifications

--- knnServer.cpp
+++ knnServer.cpp
@@ -7,6 +7,28 @@
 #include <string.h>
 using namespace std;
 /**
+ * Check if port is valid: If can be converted to int, and is in range 0-65535
+ * @param port port to listen on
+ * @return port number if exists, -1 otherwise.
+ */
+int getPort(string port) {
+    int size = port.size();
+    for (int i = 0; i < size; i++) {
+        if (isdigit(port[i]) == false) {
+            perror("Port must be an integer");
+            return -1;
+        }
+    }
+    int serverPort = stod(port);
+    if (serverPort > 65535) {
+        return -1;
+    }
+    if (serverPort < 0) {
+        return -1;
+    }
+    return serverPort;
+}
+/**
  * KNN Server.
  * @param argc count of command line args
  * @param argv command line args:
@@ -18,10 +40,14 @@ using namespace std;
  * @return nothing
  */
 int main(int argc, char *argv[]) {
-    const int server_port = 43931;
+    const int server_port = getPort(argv[2]);                                                          //Port validation
+    if (server_port == -1) {
+        cout << "Please enter a valid port" << endl;
+        server_port = getPort(argv[2]);
+    }
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        perror("error creating socket");
+        perror("Error creating socket");
     }
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
@@ -29,25 +55,30 @@ int main(int argc, char *argv[]) {
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(server_port);
     if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
-        perror("error binding socket");
+        perror("Error binding socket");
     }
     while (true) {
         if (listen(sock, 5) < 0) {
-            perror("error listening to a socket");
+            perror("Error listening to a socket");
         }
         struct sockaddr_in client_sin;
         unsigned int addr_len = sizeof(client_sin);
         int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);              //accept new connection
         if (client_sock < 0) {
-            perror("error accepting client");
+            perror("Error accepting client");
         }
         char buffer[4096];                              //TODO: receives TypeVector, ALG, K as STRING
         int expected_data_len = sizeof(buffer);
-        int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+        int read_bytes = recv(client_sock, buffer, expected_data_len, 0);                                 //Receive data
+        if (buffer[0] == '-' && buffer[1] == '1') {                                      //Is "-1" then close connection
+            cout << "Closed" << endl;
+            close(sock);
+            return 0;
+        }
         if (read_bytes == 0) {
             cout << "Closed" << endl;
         } else if (read_bytes < 0) {
-            perror("error reading from client");
+            perror("Error reading from client");
         } else {
             cout << buffer;
         }
@@ -58,6 +89,4 @@ int main(int argc, char *argv[]) {
             perror("error sending to client");
         }
     }
-    close(sock);
-    return 0;
 }

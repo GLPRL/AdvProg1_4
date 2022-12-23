d2edfd078195d61cfdb3f01dcec7f66feb7a01df
GLPRL <galpearl24@gmail.com>
2022-12-23 13:53:50 +0200

    knnServer.cpp : Works in infinite loop, added TODO to describe flow of program

--- knnServer.cpp
+++ knnServer.cpp
@@ -1,8 +1,3 @@
-/**
-* Knn server.
- * Port = 50000
- * IP = localhost
-*/
 #include <iostream>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -11,36 +6,58 @@
 #include <unistd.h>
 #include <string.h>
 using namespace std;
-int main() {
-    const int server_port = 50000;                                                                   //create new socket
+/**
+ * KNN Server.
+ * @param argc count of command line args
+ * @param argv command line args:
+ * ARG[0] = server.out
+ * ARG[1] = file to classify with
+ * ARG[2] = port
+ * TODO: All of them MUST be checked!
+ * Server receives data from client as STRING, and then divides according to whitespace the items
+ * @return nothing
+ */
+int main(int argc, char *argv[]) {
+    const int server_port = 43931;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        perror ("Error binding socket");
+        perror("error creating socket");
     }
-    if (listen(sock, 5) < 0) {                                                                                  //listen
-        perror ("Error listening on socket");
+    struct sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(server_port);
+    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+        perror("error binding socket");
     }
-    struct sockaddr_in client_sin;
-    unsigned int addr_len = sizeof(client_sin);
-    int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);                  //accept new connection
-    if (client_sock < 0) {
-        perror ("Error accepting client");
-    }
-    char buffer[2048];
-    int expected_data_len = sizeof(buffer);
-    while (true) {                                                              //infinite read data from the connection
+    while (true) {
+        if (listen(sock, 5) < 0) {
+            perror("error listening to a socket");
+        }
+        struct sockaddr_in client_sin;
+        unsigned int addr_len = sizeof(client_sin);
+        int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);              //accept new connection
+        if (client_sock < 0) {
+            perror("error accepting client");
+        }
+        char buffer[4096];                              //TODO: receives TypeVector, ALG, K as STRING
+        int expected_data_len = sizeof(buffer);
         int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-        if (read_bytes < 0) {
-            perror("Error reading data");
-        } else {                                                 //if no error reading data, then initiate knn algorithm
-            cout << buffer << endl;
-            //TODO: CALCULATIONS
-            //string data = ???
+        if (read_bytes == 0) {
+            cout << "Closed" << endl;
+        } else if (read_bytes < 0) {
+            perror("error reading from client");
+        } else {
+            cout << buffer;
         }
-        int sent_bytes = send(client_sock, "2", read_bytes, 0);
+        char outBuffer[4096];
+        // outBuffer = the result of knn
+        int sent_bytes = send(client_sock, outBuffer, read_bytes, 0);
         if (sent_bytes < 0) {
-            perror("Error sending to client");
+            perror("error sending to client");
         }
     }
+    close(sock);
     return 0;
 }
